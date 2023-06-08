f182fa10cb92ef50f9fdbab379a28928c1061f9d
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-08 16:12:07 +0200

    Added pivot table (language-project)

---
+++ database/migrations/2023_06_08_140805_create_language_project_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language_project', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('language_id');
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('language_id')->references('id')->on('languages')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('language_project');
+    }
+};
