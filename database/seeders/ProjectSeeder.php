fa061581102e276422956f2a44762fb7882bbc84
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-08 15:48:12 +0200

    Removed languages from Projects table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -21,7 +21,6 @@ class ProjectSeeder extends Seeder
             [
                 'name' => 'Boolflix',
                 'image_url' => 'https://i.ibb.co/1bL15zM/boolflix.png',
-                'languages' => 'Vue, Javascript, HTML, SCSS',
                 'tags' => 'Vue, portfolio',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis vel ante quis fermentum. In iaculis mauris lorem, aliquam viverra ex ornare a. Proin pretium dolor metus, quis consequat velit facilisis vitae. Etiam maximus id sem sit amet fermentum. Fusce eros nisl, consectetur a sem non, egestas ultrices purus. Quisque faucibus, metus non egestas interdum, odio diam hendrerit turpis, tristique interdum nunc velit eget erat.',
                 'repo_url' => 'https://github.com/raffaelecapaldo/vite-boolflix'
@@ -29,7 +28,6 @@ class ProjectSeeder extends Seeder
             [
                 'name' => 'Laravel Comics',
                 'image_url' => 'https://wallpaperaccess.com/full/2379729.jpg',
-                'languages' => 'PHP, HTML, SCSS',
                 'tags' => 'Laravel',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis vel ante quis fermentum. In iaculis mauris lorem, aliquam viverra ex ornare a. Proin pretium dolor metus, quis consequat velit facilisis vitae. Etiam maximus id sem sit amet fermentum. Fusce eros nisl, consectetur a sem non, egestas ultrices purus. Quisque faucibus, metus non egestas interdum, odio diam hendrerit turpis, tristique interdum nunc velit eget erat.',
                 'repo_url' => 'https://github.com/raffaelecapaldo/vite-comics'
@@ -37,7 +35,6 @@ class ProjectSeeder extends Seeder
             [
                 'name' => 'Campominato',
                 'image_url' => 'https://wallpapers.com/images/hd/red-forest-trees-n8z3bf3dv0b2cj6w.jpg',
-                'languages' => 'Javascript, HTML, CSS',
                 'category_id' => '2',
                 'tags' => 'Games',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis vel ante quis fermentum. In iaculis mauris lorem, aliquam viverra ex ornare a. Proin pretium dolor metus, quis consequat velit facilisis vitae. Etiam maximus id sem sit amet fermentum. Fusce eros nisl, consectetur a sem non, egestas ultrices purus. Quisque faucibus, metus non egestas interdum, odio diam hendrerit turpis, tristique interdum nunc velit eget erat.',
@@ -50,7 +47,6 @@ class ProjectSeeder extends Seeder
             $newProject->name = $project['name'];
             $newProject->slug = Str::slug($project['name'], '-');
             $newProject->image_url = $project['image_url'];
-            $newProject->languages = $project['languages'];
             $newProject->tags = $project['tags'];
             $newProject->description = $project['description'];
             $newProject->repo_url = $project['repo_url'];
